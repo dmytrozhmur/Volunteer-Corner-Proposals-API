0ced8a4c6205bc71b5ad5387b8066e369c31ea3d
Denis Rubel <dmytro.zhmur@nure.ua>
2022-10-26 23:04:11 +0300

    First repository added

--- proposalservice/src/main/java/ua/nure/proposalservice/model/User.java
+++ proposalservice/src/main/java/ua/nure/proposalservice/model/User.java
@@ -6,19 +6,19 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import java.util.List;
 
-@Getter
-@Setter
-@Entity(name = "AspNetUsers")
-public class User {
-    private String firstName;
-    private String lastName;
-    private String patronymic;
-    private String login;
-    private String email;
-    private String password;
-    private String phone;
-    private boolean blocked;
-    private boolean confirmed;
-    private List<Role> roles;
-    private String organizationId;
-}
+//@Getter
+//@Setter
+//@Entity(name = "AspNetUsers")
+//public class User {
+//    private String firstName;
+//    private String lastName;
+//    private String patronymic;
+//    private String login;
+//    private String email;
+//    private String password;
+//    private String phone;
+//    private boolean blocked;
+//    private boolean confirmed;
+//    private List<Role> roles;
+//    private String organizationId;
+//}
