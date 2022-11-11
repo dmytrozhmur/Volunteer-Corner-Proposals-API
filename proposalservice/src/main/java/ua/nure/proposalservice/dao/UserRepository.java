ddf89838d30c6ff465a4cbc6eb375bf3d1d2187f
Denis Rubel <dmytro.zhmur@nure.ua>
2022-11-11 18:28:19 +0200

    Created By throws exception

--- proposalservice/src/main/java/ua/nure/proposalservice/dao/UserRepository.java
+++ proposalservice/src/main/java/ua/nure/proposalservice/dao/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ua.nure.proposalservice.model.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    User findByLogin(String login);
 }
