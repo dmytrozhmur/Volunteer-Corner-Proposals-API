7d5c3fe2cf8583a0ddfca09a831ade1015d14967
arturlarkin <artur.larkin@nure.ua>
2022-11-14 20:41:22 +0200

    ApiRequestException added

---
+++ proposalservice/src/main/java/ua/nure/proposalservice/controller/ApiRequestException.java
@@ -0,0 +1,12 @@
+package ua.nure.proposalservice.controller;
+
+public class ApiRequestException extends RuntimeException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
